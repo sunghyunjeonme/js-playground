8378abaea171eb020ec7f23fd13d5627c4c0e77d
sunghyunjeonme <4dallove@gmail.com>
2021-02-11 23:13:08 +0900

    add :  to-do-list 기능 구현 중

---
+++ chromeapp/todo.js
@@ -0,0 +1,7 @@
+const toDoform = document.querySelector(".js-toDoForm");
+const toDoinput = form.querySelector("ul ");
+const toDoList = document.querySelector(".js-toDoList");
+
+function init() {}
+
+init();
