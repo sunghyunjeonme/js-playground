d0554f2c305c8d63b22a0146e950109367633c49
sunghyunjeonme <4dallove@gmail.com>
2021-03-09 20:08:28 +0900

    Refactor : load 이벤트 핸들링을 통한 랜덤 이미지 로드

--- chromeapp/bg.js
+++ chromeapp/bg.js
@@ -2,11 +2,15 @@ const body = document.querySelector("body");
 
 const IMG_NUMBER = 5;
 
+function handleImgLoad(image) {
+  image.classList.add("bgImage");
+  body.prepend(image);
+}
+
 function paintImage(imgNumber) {
   const image = new Image(); // object 생성
   image.src = `/chromeapp/images/${imgNumber + 1}.jpg`;
-  image.classList.add("bgImage");
-  body.appendChild(image);
+  image.addEventListener("load", handleImgLoad(image));
 }
 
 function genRandom() {
