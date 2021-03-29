3a8de9ad218e7792f0166661475a5ee9cf401f95
sunghyunjeonme <4dallove@gmail.com>
2021-03-29 23:01:45 +0900

    add : getWeather 함수 추가, 외부 API 호출

--- chromeapp/weather.js
+++ chromeapp/weather.js
@@ -1,7 +1,28 @@
+const API_KEY = "9dee0be66c49a73d09467a2844f1d34e";
 const COORDS = "coords";
 
+// 자바스크립트를 통해서 특정 URL을 호출하기
+
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`
+  );
+}
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+  console.log(coordsObj);
+}
+
 function handleGeoSucces(position) {
-  console.log(position);
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -17,7 +38,8 @@ function loadCoords() {
   if (loadedCoords === null) {
     askForCoords();
   } else {
-    // get Weather
+    const parseCoords = JSON.parse(loadedCoords);
+    getWeather(parseCoords.latitude, parseCoords.longitude);
   }
 }
 function init() {
